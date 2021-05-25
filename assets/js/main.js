f2ebaae677aa6a0efc1b66c5a5dbe1621263d8e4
Cristian Camilo <tiancamilo59@gmail.com>
2021-05-24 23:31:28 -0500

    Each of the portfolio sections was developed

---
+++ assets/js/main.js
@@ -0,0 +1,99 @@
+/*===== SHOW MENU =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction ))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageXOffset
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== CHANGE BACKGROUND HEADER =====*/ 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    const navImage = document.getElementById('header__logo')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else  header.classList.remove('scroll-header') 
+    if(this.scrollY >= 200) navImage.src="./assets/img/logos/LogoTianNegro.png"; else  navImage.src="./assets/img/logos/LogoTianBlanco.png"
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*===== SHOW SCROLL TOP =====*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else  scrollTop.classList.remove('show-scroll') 
+}
+window.addEventListener('scroll', scrollTop)
+
+/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
+
+/* Link active portfolio */ 
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
+
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
+
+/*===== SWIPER CAROUSEL =====*/ 
+const swiper = new Swiper('.testimonial__container', {
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 2,     
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+  });
+
+/*===== GSAP ANIMATION =====*/ 
